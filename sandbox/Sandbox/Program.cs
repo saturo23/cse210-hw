f83b8ffe21c432fdcaeed3c904bed8878d4ceb1f
saturo23 <mmashanda@byupathway.edu>
2025-11-01 00:46:06 +0200

    change to an affirmation

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! Sandbox World!!!.");
+        Console.WriteLine("Hello World! l am winning in all areas of my life amen.");
     }
 }
