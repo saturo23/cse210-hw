d0850d99ed9e6485f0327f5a9612f626288f02a2
saturo23 <mmashanda@byupathway.edu>
2025-11-01 00:40:48 +0200

    rewrote the massage

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! l believe in myself!!.");
+        Console.WriteLine("Hello World! Sandbox World!!!.");
     }
 }
