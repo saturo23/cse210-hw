9dc5a7701e30f2a9317f2479380b0d426b834dcb
saturo23 <mmashanda@byupathway.edu>
2025-11-01 10:29:38 +0200

    added grades

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("");
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+        Console.Write("");
+        Console.WriteLine("What is your last name? ");
+        string Surname = Console.ReadLine();
+        Console.WriteLine($"Your name is {Surname}, {name} {Surname}! Welcome to the Exercise1 Project.");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your letter grade is A.");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your letter grade is B.");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your letter grade is C.");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your letter grade is D.");
+        }
+        else
+        {
+            Console.WriteLine("Your letter grade is F.");
+        }
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! you have passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you have not passed the course. Try harder next time!");
+        }
     }
 }
