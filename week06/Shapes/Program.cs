18188d39383d24ced1b12c130658c25bb0a11efe
saturo23 <mmashanda@byupathway.edu>
2025-12-04 09:36:21 +0200

    shapesProg

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Create shapes
+        Square s = new Square("Red", 5);
+        Rectangle r = new Rectangle("Blue", 4, 6);
+        Circle c = new Circle("Green", 3);
+
+        // Add them to a list of Shapes
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(s);
+        shapes.Add(r);
+        shapes.Add(c);
+
+        // Display areas using polymorphism
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+            Console.WriteLine("-----------------------");
+        }
     }
 }
---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,17 @@
+public abstract class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    // Abstract method (must be overridden)
+    public abstract double GetArea();
+}
