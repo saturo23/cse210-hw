84914a70e501538169b5bcd6ad5e6e79803b179a
saturo23 <mashandamduduzi@gmail.com>
2026-02-18 02:51:30 +0200

    added a comment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        //its a simple program that asks the user for their first and last name and then greets them with a welcome message.
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
         Console.Write("");
         Console.Write("What is your first name? ");
